45b301c40823b31487ebc142f70c599671e9f3b1
obiPlabon <obiplabon@gmail.com>
2022-06-05 13:37:18 +0600

    updated styles and improved query

--- youtube-videos-for-woocommerce.php
+++ youtube-videos-for-woocommerce.php
@@ -13,17 +13,36 @@
  *
  * @package WC_Youtube_Videos
  */
+namespace wpWax;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * WooCommerce Product Related Youtube Videos class.
+ */
 final class WC_Youtube_Videos {
 
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
 	protected $version = '1.0';
 
+	/**
+	 * Instace of WC_Youtube_Videos.
+	 *
+	 * @var WC_Youtube_Videos
+	 */
 	protected static $instance = null;
 
+	/**
+	 * Singalaton instance.
+	 *
+	 * @return WC_Youtube_Videos
+	 */
 	public static function instance() {
 		if ( is_null( self::$instance ) ) {
 			self::$instance = new self();
@@ -32,22 +51,43 @@ final class WC_Youtube_Videos {
 		return self::$instance;
 	}
 
+	/**
+	 * Constructor
+	 */
 	private function __construct() {
 		$this->init();
 	}
 
+	/**
+	 * Initialize the plugin.
+	 *
+	 * @return void
+	 */
 	public function init() {
 		add_action( 'woocommerce_init', array( $this, 'setup_hooks' ) );
 	}
 
+	/**
+	 * Setup plugin hooks.
+	 *
+	 * @return void
+	 */
 	public function setup_hooks() {
-		add_filter( 'woocommerce_product_tabs', array( $this, 'add_product_tab' ) );
+		add_action( 'save_post', array( $this, 'cache_videos' ) );
 		add_filter( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_filter( 'woocommerce_product_tabs', array( $this, 'add_product_tab' ) );
 	}
 
+	/**
+	 * Register custom product tab.
+	 *
+	 * @param  array $tabs
+	 *
+	 * @return array Modified array with the custom tab.
+	 */
 	public function add_product_tab( $tabs ) {
-		$tabs['youtube_videos'] = array(
-			'title' 	=> __( 'Videos', 'woocommerce' ),
+		$tabs['ytwc'] = array(
+			'title' 	=> __( 'Videos', 'youtube-videos-for-woocommerce' ),
 			'priority' 	=> 50,
 			'callback' 	=> array( $this, 'render_product_tab' )
 		);
@@ -55,42 +95,51 @@ final class WC_Youtube_Videos {
 		return $tabs;
 	}
 
+	/**
+	 * Render product custom tab.
+	 *
+	 * @return void
+	 */
 	public function render_product_tab() {
 		global $product;
 
-		// print_r( $this->get_videos( $product->get_title() ) );
+		$videos = $this->get_videos( $product->get_id() );
+		if ( $videos ) :
 		?>
 		<div class="ytwc-grid">
+			<?php
+			foreach ( $videos as $video ) :
+				$url = 'https://www.youtube.com/embed/' . $video['id']['videoId'];
+				?>
 			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<iframe loading="lazy" width="560" height="315" data-src="<?php echo esc_url( $url ); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<div class="ytwc-grid__item">
-				<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/WrhjbdWDpTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
+			<?php endforeach; ?>
 		</div>
+		<script>
+			var ytwc = document.querySelector('#tab-title-ytwc');
+			if (ytwc) {
+				ytwc.addEventListener('click', function(event) {
+					var iframes = document.querySelectorAll('.ytwc-grid__item > iframe');
+					if (iframes) {
+						iframes.forEach(function(iframe) {
+							iframe.setAttribute('src', iframe.getAttribute('data-src'));
+						});
+					}
+				});
+			}
+		</script>
 		<?php
+		endif;
 	}
 
+	/**
+	 * Enqueue assets.
+	 *
+	 * @return void
+	 */
 	public function enqueue_scripts() {
-		if ( is_product() && $this->has_videos() ) {
+		if ( is_product() ) {
 			wp_enqueue_style(
 				'ytwc-style',
 				plugin_dir_url( __FILE__ ) . 'assets/css/style.css',
@@ -100,25 +149,72 @@ final class WC_Youtube_Videos {
 		}	
 	}
 
-	public function has_videos() {
-		return true;
-	}
-
-	public function get_videos( $query_string ) {
+	public function query_videos( $query_string ) {
 		$api_key = 'AIzaSyDX2n54ulhfNRFu7gr7-JSSK_2t01bs5Qo';
 		$api_url = 'https://www.googleapis.com/youtube/v3/search';
 		$api_url = add_query_arg(
 			array(
-				'key' => $api_key,
-				'q'   => $query_string,
+				'key'             => $api_key,
+				'q'               => $query_string,
+				'maxResults'      => 4,
+				'type'            => 'video',
+				'videoDuration'   => 'short',
+				'videoEmbeddable' => true,
 			),
 			$api_url
 		);
 
-		$response = wp_remote_get( $api_url );
+		$response = wp_remote_get( esc_url_raw( $api_url ) );
 		$response = wp_remote_retrieve_body( $response );
 
-		return $response;
+		return ( $response ? json_decode( $response, true ) : array() );
+	}
+
+	public function get_videos( $product_id = 0 ) {
+		$data = $this->get_videos_data( $product_id );
+
+		return ( ! empty( $data['items'] ) ? $data['items'] : array() );
+	}
+
+	public function get_videos_data( $product_id = 0 ) {
+		$product = wc_get_product( $product_id );
+
+		if ( ! $product ) {
+			return array();
+		}
+
+		$videos = get_post_meta( $product_id, '_ytwc_cache', true );
+
+		if ( empty( $videos ) ) {
+			$videos = $this->query_videos( strip_tags( $product->get_title() ) );
+
+			if ( ! empty( $videos ) ) {
+				update_post_meta( $product_id, '_ytwc_cache', $videos );
+
+				$videos = get_post_meta( $product_id, '_ytwc_cache', true );
+			}
+		}
+
+		return $videos;
+	}
+
+	public function cache_videos( $post_id ) {
+		// Autosaving, bail.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		if ( get_post_type( $post_id ) !== 'product' ) {
+			return;
+		}
+
+		$product = wc_get_product( $post_id );
+		if ( ! $product ) {
+			return;
+		}
+
+		delete_post_meta( $post_id, '_ytwc_cache' );
+		$this->get_videos( $post_id );
 	}
 }
 
