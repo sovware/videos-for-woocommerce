d9fe084e39f10f76db7632d3d307807321581549
obiPlabon <obiplabon@gmail.com>
2022-06-21 12:28:28 +0600

    shellbang fix

--- bin/make-pot.sh
+++ bin/make-pot.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#!/bin/bash
 
 wp i18n make-pot . languages/videos-for-woocommerce.pot --exclude="tests,wp,vendor" --skip-js --domain="videos-for-woocommerce" --slug="videos-for-woocommerce"
